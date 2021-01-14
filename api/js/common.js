5ee0c574227a5a2a66b2d4631104827c3e82be44
Alejo Moreira <alejo.moreira@729solutions.com>
2021-01-14 18:29:45 -0300

    Add files via upload

---
+++ api/js/common.js
@@ -0,0 +1,20 @@
+
+
+var getAccessToken = function(auth_url, auth_url) {
+  return new Promise((resolve, reject) => { 
+    request.post({url: auth_url, headers: auth_headers}, function(error, response, body){
+      if (response.statusCode != 200) {
+        throw new Error('Error: Response Status Code ' + response.statusCode)
+      }
+      if (error) {
+        throw new Error('Error: ' + error)
+      }
+      var info = JSON.parse(body);
+      if (info.access_token == undefined) {
+        throw new Error('Error: No access token')
+      }
+      resolve(info.access_token);
+    });
+  }).catch((e) => { console.error(e); throw new Error(e) })
+}
+
